d20f2bf5250013bbf6017aa0b57a5778982f62b9
Matteo <Rascioni>
2020-10-16 15:40:40 +0200

    100%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,71 @@
+// SNACK 1
+// var num1 = parseInt(prompt('inserisci il primo numero' ) );
+// var num2 = parseInt(prompt('inserisci il secondo numero' ) );
+
+// if (num1 > num2) {
+//     document.getElementById('printing').innerHTML = 'Il primo numero è quello più alto: ' + num1;
+// } else if (num2 > num1) {
+//     document.getElementById('printing').innerHTML = 'Il secondo numero è quello più alto: ' + num2;
+// } else {
+//     document.getElementById('printing').innerHTML = 'Gli elementi hanno lo stesso valore: ' + num1 + ' / ' + num2;
+// }
+
+// SNACK 2
+// var parolaA = prompt('Inserisci la prima parola');
+// var parolaB = prompt('Inserisci la seconda parola');
+
+// if (parolaA.length > parolaB.length) {
+//     document.getElementById('printing').innerHTML = 'La prima parola è quella più lunga: ' + parolaA;
+// } else if (parolaA.length < parolaB.length) {
+//     document.getElementById('printing').innerHTML = 'La seconda parola è quella più lunga: ' + parolaB;
+// } else { 
+//     document.getElementById('printing').innerHTML = 'Le due parole sono lunghe uguali: ' + parolaA + ' / ' + parolaB;
+// }
+
+// SNACK 3 
+//  var somma = 0;
+
+//  for (var i = 0; i <= 4; i++) {
+//      var number = parseInt(prompt('Inserisci un numero') );
+
+//      somma += number;
+
+//      document.getElementById('printing').innerHTML = 'Totale: ' + somma; 
+//  }
+
+// SNACK 4 
+// var arrayGuest = ['Matteo', 'Paolo', 'Fabio', 'Lorenzo', 'Mattia', 'Manuel', 'livia'];
+
+// var message = prompt('Inserisci il nome per verificare l\'invito');
+
+// var guest = false;
+
+// for (var i = 0; i < arrayGuest.length; i++) { 
+
+//     if(arrayGuest[i] == message) {
+//         guest = true;
+//     }
+// }
+
+// if(guest == true) {
+//     document.getElementById('printing').innerHTML = 'Benvenuto alla festa di MR. Gatsby può entrare';
+    
+// } else {
+//     document.getElementById('printing').innerHTML = 'Ci dispiace lei non è sulla lista, non può entrare';
+// }
+
+// SNACK 5
+// var arrayVuoto = [];
+
+// for (i = 0; i < 6; ++i) {
+//     var number = parseInt(prompt('Inserisci un numero')); 
+    
+//     if (number % 2 != 0) {
+//     arrayVuoto.push (number)    
+//         document.getElementById('printing').innerHTML = 'Il tuo numero dispari è: ' + arrayVuoto;
+//     }
+// }
+
+
+
 
